6c34a7d0877b222add61d496b18b1f3218dc58a3
Henrique <henriquearnaud@live.com>
2020-12-08 16:04:44 -0300

    conexao.php movida

--- Controller/conexao.php
+++ Controller/conexao.php
@@ -5,6 +5,12 @@
     $banco = 'emailmarketing';
 
     $con = new mysqli($host,$usuario,$senha,$banco);
+    if($con){
+      echo "BELEZA";
+    }
+    else{
+      echo "NAO BELEZA";
+    }
     if(mysqli_connect_errno()){
 		exit("Erro ao conectar-se ao banco de dados: ".mysqli_connect_error());
     }
