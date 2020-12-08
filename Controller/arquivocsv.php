99a05645bf1064cd2258a319db2ea02f4bb4ecf6
Lucas Marcelo <lucasdeveloper98l@gmail.com>
2020-12-08 15:53:46 -0300

    Polando Banco de Dados

--- Controller/arquivocsv.php
+++ Controller/arquivocsv.php
@@ -14,5 +14,17 @@ class ArquivosCsv{
     fclose($arquivo);    
     }
 
+    function populaBanco(){
+        $host = 'localhost';
+        $usuario = 'root';
+        $senha = '';
+        $banco = 'emailmarketing';
+
+        $con = new mysqli($host,$usuario,$senha,$banco);
+        if(mysqli_connect_errno()){
+		    exit("Erro ao conectar-se ao banco de dados: ".mysqli_connect_error());
+        }
+    }
+
 }
 ?>
--- db/conexao.php
+++ db/conexao.php
@@ -7,5 +7,5 @@
     $con = new mysqli($host,$usuario,$senha,$banco);
     if(mysqli_connect_errno()){
 		exit("Erro ao conectar-se ao banco de dados: ".mysqli_connect_error());
-	}
+    }
 ?>
